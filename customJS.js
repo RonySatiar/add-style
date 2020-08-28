4deda8457d88fc973e4abbe53b884d18bf507653
Golak Chandra Satiar <rsatiareee@gmail.com>
2020-08-28 17:43:53 +0600

    style update

---
+++ customJS.js
@@ -0,0 +1,21 @@
+(function() {
+$("<h2 class='main-subheading'>Organize your money effectively<br>with our helpful guides.</h2>").insertAfter("#docsSearch h1");
+
+$("#contentArea").before("<div class='popular-article-holder'>\
+  <h3>Popular Articles</h3>\
+  <ul>\
+    <li>\
+      <a href='#'>Craigslist Best Practices</a>\
+      <a href='#'>Selecting Photos for Craigslist</a>\
+    </li>\
+    <li>\
+      <a href='#'>Craigslist Best Practices</a>\
+      <a href='#'>Selecting Photos for Craigslist</a>\
+    </li>\
+    <li>\
+      <a href='#'>Craigslist Best Practices</a>\
+      <a href='#'>Selecting Photos for Craigslist</a>\
+    </li>\
+  </ul>\
+</div>");
+  })()
